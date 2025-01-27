83279ecafb4fd0eedc6daf3a88318b12ec26db50
EvaBytes <evasevillanop@gmail.com>
2025-01-27 16:57:01 +0100

    Room Test Update

---
+++ Testing/room.test.js
@@ -0,0 +1,13 @@
+
+
+describe('Room functionality', () => {
+    describe('Room creation', () => {
+      it('should create a room', () => {
+
+            });
+  
+      it('should not create a room with the same name', () => {
+      });
+    });
+  });
+  
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node', 
+    collectCoverage: true, 
+    coverageDirectory: 'coverage', 
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], 
+  };
+  
